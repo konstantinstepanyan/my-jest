b8d21bdd45ecb6e00b95f09e4a723bf8e05531fb
Konstantin <konstantinstepanyan99@gmail.com>
2021-10-30 04:32:46 +0300

    done lesson 2

---
+++ src/Post/post.spec.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Post from "./post";
+
+const setUp = (props) => shallow(<Post {...props} />);
+
+
+describe("Should render Post component", () => {
+    let component;
+    
+    beforeEach(()=>{
+    component = setUp();
+    });
+    
+    it("should contain .post wrapper", () => {
+        const wrapper = component.find(".post");
+        expect(wrapper.length).toBe(1);
+        console.log(component.debug())
+    })
+
+
+    it("should contain a (link)", () => {
+        const wrapper = component.find("a");
+        expect(wrapper.length).toBe(1);
+        console.log(component.debug())
+    })
+
+    it("should render created date", () => {
+        const created_at = "01-03-2020";
+        component = setUp({created_at});
+        const date = component.find(".date");
+        expect(date.text()).toBe(new Date(created_at).toLocaleDateString())
+
+    })
+
+});
+
